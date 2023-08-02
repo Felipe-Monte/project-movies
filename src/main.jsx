20112eb5a2da3f2ff5e387dd2509f61c90201b0e
felipe <carlosfelipemonte1997@hotmail.com>
2023-08-02 14:14:11 -0300

    project-movies

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './styles/global'
+import theme from './styles/theme'
+
+
+import { App } from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
